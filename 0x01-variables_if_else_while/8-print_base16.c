115db0520adac47d18d0e9e90754efd366e28317
Olutunde <solabi.tunde@gmail.com>
2022-03-13 23:22:48 +0000

    zz

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the 26 aplhabets in lowercase in base 16
+ * main - Prints numbers between 0 to 9 and letters between a to f.
  *
- * Return: 0 as Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-    
-    for (i = 0; i <= 9; i++)
+
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 	}
-    for (i = 0; i <= 5; i++)
+	for (i = 0; i <= 5; i++)
 	{
 		putchar(alphabet[i]);
 	}
