1039d035fcfd5d0ea7a1387694a6d77cfb061d2f
Olutunde <solabi.tunde@gmail.com>
2022-03-13 21:46:28 +0000

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,12 @@ int main(void)
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
-        if(alphabet[i] != 'p' || alphabet[i] != 'e'){
-            putchar(alphabet[i]);
-        }
-		
+		if (alphabet[i] != 'e' && alphabet[i] != 'q')
+		{
+			putchar(alphabet[i]);
+		}
 	}
 	putchar('\n');
 	return (0);
