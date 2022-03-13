5a8c571436fcbf50e7d457a7a093a657b80fbe8d
Olutunde <solabi.tunde@gmail.com>
2022-03-13 23:16:44 +0000

    zz

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-
+    
     for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
 	}
-    for (i = 0; i <= 9; i++)
+    for (i = 0; i <= 5; i++)
 	{
 		putchar(alphabet[i]);
 	}
