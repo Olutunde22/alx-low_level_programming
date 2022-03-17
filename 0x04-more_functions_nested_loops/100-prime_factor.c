409b66aff964cb9f6bc77c28867f631e08e47923
Olutunde <solabi.tunde@gmail.com>
2022-03-17 19:57:38 +0000

    print  number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, j;
+
+	n = 612852475143;
+	for (j = 2; j <= n; j++)
+	{
+		if (n % j == 0)
+		{
+			n /= j;
+			j--;
+		}
+	}
+	printf("%ld\n", j);
+	return (0);
+}
